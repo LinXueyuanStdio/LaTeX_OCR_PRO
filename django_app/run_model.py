ce89ca18c9ba24cc2127f46a508d07489a276513
兮尘 <761516186@qq.com>
2019-07-25 11:42:37 +0800

    Update run_model.py

--- django_app/run_model.py
+++ django_app/run_model.py
@@ -8,7 +8,7 @@ from model.manager import ModelManager
 def predict_by_image_filename(image_filename="2.png"):
     IMAGE_PATH = "data/images_train/"+image_filename
     try:
-        return ModelManager.instance().vis_png(IMAGE_PATH)
+        return ModelManager.instance().predict_png(IMAGE_PATH)
     except:
         return 'something wrong happend'
 
@@ -24,10 +24,12 @@ def search(request):
     print(request.GET)
     if len(request.GET) > 0:
         image_url = request.GET['info']  # “0.png”
-        a = predict_by_image_filename(image_url)
-        predict_string, gif_path = a[0], a[1]
+        # a = predict_by_image_filename(image_url)
+        # predict_string, gif_path = a[0], a[1]
+        predict_string = predict_by_image_filename(image_url)
         image_url = "http://localhost:8020/" + image_url.split("/")[-1].split("\\")[-1]
-        image_url = "http://localhost:8030/" + gif_path.split("/")[-1].split("\\")[-1]
+        gif_path = image_url
+        # gif_path = "http://localhost:8030/" + gif_path.split("/")[-1].split("\\")[-1]
         return render_to_response('search_result.html', {
             'image_url': image_url,
             'gif_path': gif_path,
