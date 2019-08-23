e0f099de3cf8e7e196287c7431661f74968ac2bc
兮尘 <761516186@qq.com>
2019-08-23 14:56:46 +0800

    add adadelta lr method

--- model/img2seq_2.py
+++ model/img2seq_2.py
@@ -102,6 +102,8 @@ class Img2SeqModel(BaseModel):
                 optimizer = tf.train.AdamOptimizer(lr)
             elif _lr_m == 'adagrad':
                 optimizer = tf.train.AdagradOptimizer(lr)
+            elif _lr_m == 'adadelta':
+                optimizer = tf.train.AdadeltaOptimizer(lr)
             elif _lr_m == 'sgd':
                 optimizer = tf.train.GradientDescentOptimizer(lr)
             elif _lr_m == 'rmsprop':
