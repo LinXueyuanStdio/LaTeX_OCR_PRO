5a48dc916b9aa07c99135ff3118b82ba29939c32
R. He <65575855+RonghaiHe@users.noreply.github.com>
2024-03-04 16:49:59 +0800

    Fix a bug in text.py
    sentence for print outside the loop may not contain loop variable "i"

--- model/utils/text.py
+++ model/utils/text.py
@@ -135,7 +135,8 @@ def write_vocab(vocab, filename):
                 f.write("{}\n".format(word))
             else:
                 f.write(word)
-        print("- done. {} tokens".format(i+1))
+        # print("- done. {} tokens".format(i+1))
+    print("- done. {} tokens".format(len(vocab)))
 
 
 def pad_batch_formulas(formulas, id_pad, id_end, max_len=None):
